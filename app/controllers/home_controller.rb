eff2cfbc655def95a3a03a554bec56558a32eb91
bwu <brian.george.wu@gmail.com>
2011-10-12 15:57:38 -0400

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+  	@posts = Post.order("created_at desc").limit(10)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swootsblog::Application.config.secret_token = '5c24706ffb96cd22c5459bc416c59d3c9562c0aa8492e3551d754375d47db43e2d152ec10f607b408f271a352fe645325eca502f33670a26b405a205049f9680'
